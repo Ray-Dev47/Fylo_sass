579a508adc84e0b81bf301b8aba64f960d40de08
Raymond Idengeli <ray.idengeli@gmail.com>
2022-11-04 17:54:12 +0100

    added local and session storage to lighg and dark mode toggle

--- script.js
+++ script.js
@@ -38,21 +38,36 @@ buttons.forEach((button) => {
 const checkbox = document.getElementById('checkbox');
 
 
-
-
-checkbox.addEventListener('click', (e) => {
-	if (e.target) {
-		console.log('hey clicked add');
-		document.body.classList.toggle('dark');	
-        localStorage.setItem('dark', false);
-        localStorage.getItem('dark')
-	} 
-    else {
-		localStorage.removeItem('dark');
-        console.log('hey clicked remove');
-        document.body.classList.remove('dark');
-	}
-});
+//check storage if dark mode was on or off
+if (sessionStorage.getItem("mode") == "dark") {
+    darkmode(); //if dark mode was on, run this funtion
+  } else {
+    nodark(); //else run this funtion
+  }
+  
+  //if the checkbox state is changed, run a funtion
+  checkbox.addEventListener("change", function() {
+    //check if the checkbox is checked or not
+    if (checkbox.checked) {
+      darkmode(); //if the checkbox is checked, run this funtion
+    } else {
+      nodark(); //else run this funtion
+    }
+  });
+  
+  //function for checkbox when checkbox is checked
+  function darkmode() {
+    document.body.classList.add("dark"); //add a class to the body tag
+    checkbox.checked = true; //set checkbox to be checked state
+    sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
+  }
+  
+  //function for checkbox when checkbox is not checked
+  function nodark() {
+    document.body.classList.remove("dark"); //remove added class from body tag
+    checkbox.checked = false; //set checkbox to be unchecked state
+    sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
+  }
 
 
 // get updated year
